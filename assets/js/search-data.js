fb7832faad5d655a49b936a2e66f1361e2bbc38f
hpliddell <40674373+hpliddell@users.noreply.github.com>
2024-12-30 15:17:23 +0000

    Deploying to gh-pages from @ hpliddell/hpliddell.github.io@2225384d151f15a8490f9262191b103a7ac6fd4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,6 +490,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/heather-liddell", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.purdue.edu/", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
